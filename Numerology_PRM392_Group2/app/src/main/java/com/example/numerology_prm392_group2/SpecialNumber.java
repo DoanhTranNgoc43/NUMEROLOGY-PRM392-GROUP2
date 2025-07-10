6607ad9eb66b87f17deb8ae8ab063005ff36e7a2
toanpkhe173035 <toanpkhe173035@fpt.edu.vn>
2025-07-10 21:18:50 +0700

    SpecialNumber

---
+++ Numerology_PRM392_Group2/app/src/main/java/com/example/numerology_prm392_group2/SpecialNumber.java
@@ -0,0 +1,14 @@
+package com.example.numerology_prm392_group2;
+
+public class SpecialNumber {
+    private  String number;
+    private double totalAmount;
+    private int ticketCount;
+    private String riskLevel;
+
+//    public SpecialNumber(String number, double totalAmount, int ticketCount) {
+//
+
+    }
+
+
