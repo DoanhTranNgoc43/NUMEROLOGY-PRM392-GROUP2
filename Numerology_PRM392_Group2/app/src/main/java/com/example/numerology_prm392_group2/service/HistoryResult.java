525e94c1379d13326f83e61db44c103b2f062d16
toanpkhe173035 <toanpkhe173035@fpt.edu.vn>
2025-07-13 21:17:52 +0700

    HistoryResult

---
+++ Numerology_PRM392_Group2/app/src/main/java/com/example/numerology_prm392_group2/service/HistoryResult.java
@@ -0,0 +1,7 @@
+package com.example.numerology_prm392_group2.service;
+
+public class HistoryResult {
+    private String bettorName;
+    private String bettingNumber;
+    private double bettingAmount;
+}
