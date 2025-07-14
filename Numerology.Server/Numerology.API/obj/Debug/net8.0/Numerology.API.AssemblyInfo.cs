27939e7f4c17d18f9d90d740099fc15456803b0b
trongbdhe173121 <trongbdhe173121@fpt.edu.vn>
2025-07-14 15:33:50 +0700

    fix result

--- Numerology.Server/Numerology.API/obj/Debug/net8.0/Numerology.API.AssemblyInfo.cs
+++ Numerology.Server/Numerology.API/obj/Debug/net8.0/Numerology.API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Numerology.API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+29bd145ac64107bbf28eec5cfe13fecdee06a77f")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+c9ebbce63b2bd7c1dd5e1c743336cd18912a4186")]
 [assembly: System.Reflection.AssemblyProductAttribute("Numerology.API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Numerology.API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
